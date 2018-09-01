9254edbd5eb01b7ac40a0dbd39b5b6b89621fa9f
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-08-31 22:03:25 -0400

    Update dnd_dice_roll_function.py
    I just wanted to show my first function before I decided to simplify it more.

--- dnd_dice_roll_function.py
+++ dnd_dice_roll_function.py
@@ -1,31 +1,9 @@
 import random
 
-#This was my first attempt but I figured I could simplify it more. See line 20.
-# def dice_roll(d, n):
-#     for r in range(n):
-#         if d == 20:
-#             x = random.randint(1,20)
-#         elif d == 12:
-#             x = random.randint(1, 12)
-#         elif d == 10:
-#             x = random.randint(1, 10)
-#         elif d == 8:
-#             x = random.randint(1, 8)
-#         elif d == 6:
-#             x = random.randint(1, 6)
-#         else:
-#             x = random.randint(1, 4)
-#         print(x)
-
-#dice_roll(10,5)
-
 def roll_dice(sides, rolls):
     for roll in range(rolls):
         score = random.randint(1,sides)
         print(score)
-
-#roll_dice(10, 10)
-
 
 dice_sides = int(input("How many sides does your dice have? "))
 roll_number = int(input("How many rolls do you want to make? "))
