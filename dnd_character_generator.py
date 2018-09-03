a49270bb6637e4f73b93d2785f3193fcdb338f36
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-09-03 19:48:46 -0400

    Update dnd_character_generator.py

--- dnd_character_generator.py
+++ dnd_character_generator.py
@@ -1,27 +1,37 @@
-#Since I want to have the choice between choices and randomizing, I'm importing random module.
+#Since I want to have the choice between choices and randomizing, I'm importing
+##random module.
 import random
 
 #I want to choose my class or have it randomized.
-classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+CLASSES = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk',
+           'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
 
 print('First - what class do you want your character to be?\n')
 
-for cls in classes:
+for cls in CLASSES:
     print(cls)
 
-print('\nType one of the classes listed above. \nOr Type "Random" if you want a randomized class.')
+print('\nType one of the classes listed above. \nOr Type "Random" if you want '
+      'a randomized class.')
 
-player_class = ''
+playerClass = ''
 
-while player_class == '':
-    player_class = input()
-    if player_class == 'Random':
-        player_class = random.choice(classes)
-        print(f'OK, so the class I will choose is {player_class}. Let\'s continue...')
+# This while loop is killing me, I don't know how to continue if a person 
+# enter an invalid string of text.
+#while playerClass not in CLASSES:
+while playerClass == '':
+    playerClass = input()
+    if playerClass == 'Random':
+        playerClass = random.choice(CLASSES)
+        print(f'OK, so the class I will choose is {playerClass}. Let\'s '
+              'continue...')
         continue
-    elif player_class in classes:
-        print(f'OK, so {player_class} is the class you chose. Let\'s continue...')
+    elif playerClass in CLASSES:
+        print(f'OK, so {playerClass} is the class you chose. Let\'s '
+              'continue...')
         continue
+ #   elif playerClass not in CLASSES:
+ #       playerClass = input('Try again: ')
 
 print('<<<END>>>')
 
